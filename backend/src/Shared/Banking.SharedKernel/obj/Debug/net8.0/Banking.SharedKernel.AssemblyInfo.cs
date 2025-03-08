397a8757208fb04918e82ab896457957928b0cc7
? <gusev-sasha-01@mail.ru>
2025-03-08 15:51:24 +0700

    /

--- backend/src/Shared/Banking.SharedKernel/obj/Debug/net8.0/Banking.SharedKernel.AssemblyInfo.cs
+++ backend/src/Shared/Banking.SharedKernel/obj/Debug/net8.0/Banking.SharedKernel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Banking.SharedKernel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac4adca19ec4d77fd43608fe7fb831a2b56bc965")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+031a3373e5a5f7cf22fab11597de454f779c021a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Banking.SharedKernel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Banking.SharedKernel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
